69ca221175699f8c65ffa955137ae5d2d4642959
phungkhai <phkhaikk@gmail.com>
2019-02-18 20:09:52 +0700

    add all folder

---
+++ js/sanpham.js
@@ -0,0 +1,6 @@
+var SanPham = /** @class */ (function () {
+    function SanPham(argument) {
+        // code...
+    }
+    return SanPham;
+}());
---
+++ js/sanpham.ts
@@ -0,0 +1,14 @@
+class SanPham{
+	private id : number;
+			ten : string;
+			gia : number;
+			mota : string;
+			tinhtrang : boolean;
+			anh : string;
+
+
+	
+	constructor(argument) {
+		// code...
+	}
+}
